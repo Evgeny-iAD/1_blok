96bd6f4a1aad0da64c3a03f559ea82497acf2423
iAD <iadmail1987@gmail.com>
2022-10-10 19:56:14 +0300

    add functions

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string[] StringArray = {"hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Rus", "Denmark", "Kazan"};
+
+Console.Write("[");
+Console.Write(PrintText(FindStringArray(StringArray)));
+Console.Write("]");
+
+string [] FindStringArray(string [] array)
+{
+
+  return result;
+}
+
+string PrintText(string [] array)
+{
+
+  return "-1";
+}
